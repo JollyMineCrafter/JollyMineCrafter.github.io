c902c5f6200ec0bb6ad9c38d156d0e4d2ff899e2
Jolly <masteroffryingpans@gmail.com>
2022-02-08 20:45:48 +0700

    Create app.js

---
+++ easter/app.js
@@ -0,0 +1,9 @@
+const display = document.querySelector('#name');
+const yourName = localStorage.getItem('#name');
+const anchor = document.querySelector('#switch');
+
+display.textContent = yourName;
+
+anchor.addEventListener('click', function () {
+    localStorage.removeItem('name');
+});
