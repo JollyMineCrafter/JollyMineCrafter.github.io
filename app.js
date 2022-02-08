50898a4102953f38c4c1e4d5d3709d742fb1ffe3
Jolly <masteroffryingpans@gmail.com>
2022-02-08 15:46:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
+const user = document.querySelector('#user');
+const max = user.maxLength;
+const back = document.querySelector('#return');
+const proceed = document.querySelector('#proceed');
 
+const allow = /^[a-z\d_\s]+$/i;
+
+if (user.value.length === max) { user.style.color = 'yellow' } else { user.style.color = 'white' }
+
+user.addEventListener('input', function (e) {
+    v = user.value.toLowerCase()
+    if (v === 'gaster') {
+        user.value = ''
+        user.style.color = 'unset';
+    } else if (v.length === max) {
+        user.style.color = 'yellow';
+    } else {
+        user.style.color = 'white';
+    }
+});
+
+back.addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log("GOOD NIGHT.");
+});
+
+proceed.addEventListener('click', function (e) {
+    e.preventDefault();
+    const val = user.value;
+    console.log(val);
+});
